2881d01efabc6b4a0234e8030f936b7fe3c400dc
Nicholas <nicholas.l.smith@strath.ac.uk>
2018-05-14 15:56:52 +0100

    Test method

--- app/src/main/java/com/paltechnologies/myapplication/MainActivity.java
+++ app/src/main/java/com/paltechnologies/myapplication/MainActivity.java
@@ -2,12 +2,31 @@ package com.paltechnologies.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.paltechnologies.pal8.Scanner.Scanner;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainActivity extends AppCompatActivity
+    implements Observer {
+    private final String TAG = "MainActivity";
+
+    private Scanner m_scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_scanner = new Scanner(this);
+        m_scanner.addObserver(this);
+        m_scanner.scan();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        Log.i(TAG, "update: Something found");
     }
 }
---
+++ mylibrary/src/main/java/com/paltechnologies/mylibrary/TempTest.java
@@ -0,0 +1,9 @@
+package com.paltechnologies.mylibrary;
+
+public class TempTest {
+    public TempTest() {}
+
+    public int getNumber() {
+        return 1543;
+    }
+}
